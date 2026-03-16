628a0af6cd692106756d925cc56d0a664bc50899
DawitMulugeta23 <dawitmulugetas23@gmail.com>
2026-03-16 22:43:54 +0300

    SettingLayout.jsx error fixed

---
+++ CRM/frontend/src/components/Settings/SettingsLayout.jsx
@@ -0,0 +1,11 @@
+// src/components/Settings/SettingsLayout.jsx
+
+const SettingsLayout = ({ children }) => {
+  return (
+    <div className="bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden">
+      {children}
+    </div>
+  );
+};
+
+export default SettingsLayout;
